3264e770f81b7d7145d040813bd9099407ca47b8
ton1czech <tonyasek007@gmail.com>
2026-02-17 15:35:26 +0100

    🐛 fix: gallery image dimensions

--- components/gallery/photo.tsx
+++ components/gallery/photo.tsx
@@ -1,7 +1,13 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Cursor } from '../cursor'
-import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 
@@ -9,7 +15,36 @@ interface PhotoProps {
   photo: string
 }
 
+interface PhotoDimensions {
+  width: number
+  height: number
+}
+
 export const Photo = ({ photo }: PhotoProps) => {
+  const [dimensions, setDimensions] = useState<PhotoDimensions>({
+    width: 3,
+    height: 2,
+  })
+
+  useEffect(() => {
+    let mounted = true
+    const image = new window.Image()
+
+    image.src = photo
+    image.onload = () => {
+      if (!mounted) return
+
+      setDimensions({
+        width: image.naturalWidth || 3,
+        height: image.naturalHeight || 2,
+      })
+    }
+
+    return () => {
+      mounted = false
+    }
+  }, [photo])
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -22,22 +57,24 @@ export const Photo = ({ photo }: PhotoProps) => {
             <Image
               src={photo}
               alt='photo'
-              width={1200}
-              height={800}
+              width={dimensions.width}
+              height={dimensions.height}
               sizes='(max-width: 768px) 50vw, 33vw'
-              className='rounded-lg'
+              className='h-auto w-full rounded-lg'
             />
           </Cursor>
         </DialogTrigger>
-        <DialogContent className='max-h-[90vh] max-w-[95vw]'>
-          <Image
-            src={photo}
-            alt='photo'
-            width={1920}
-            height={1280}
-            sizes='95vw'
-            className='max-h-[90vh] max-w-[95vw] rounded-lg'
-          />
+        <DialogContent className='h-[96vh] w-[98vw] max-h-[96vh] max-w-[98vw] border-none bg-transparent p-0 shadow-none sm:max-w-[98vw]'>
+          <DialogTitle className='sr-only'>Photo preview</DialogTitle>
+          <div className='relative h-full w-full overflow-hidden rounded-lg'>
+            <Image
+              src={photo}
+              alt='photo'
+              fill
+              sizes='95vw'
+              className='object-contain'
+            />
+          </div>
         </DialogContent>
       </Dialog>
     </motion.div>
