8bf6b47f6abb733337f8cd4c14880ce27118c8e4
ton1czech <tonyasek007@gmail.com>
2026-06-08 09:56:34 +0200

    🗑️ remove: test api

--- app/api/order/route.ts
+++
@@ -1,9 +0,0 @@
-import orderData from '@/data/order.json'
-
-export async function GET() {
-  return Response.json(orderData, {
-    headers: {
-      'Cache-Control': 'public, max-age=3600',
-    },
-  })
-}
